813b11f306c7338ec1f63bbac529b7dd1c86b644
Sarah Kirk <kirk710@gmail.com>
2017-06-26 17:12:01 -0600

    first test getting factory girl configured

---
+++ spec/requests/api/v1/merchants_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "sends a list of merchants" do
+    create_list(:merchants, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_success
+  end
+end
