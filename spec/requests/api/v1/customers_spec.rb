6ad1bbcf2c379df9a8f736d00dc756707ed82e21
Sarah Kirk <kirk710@gmail.com>
2017-06-26 18:08:25 -0600

    passes customers request test

--- spec/requests/api/v1/customers_spec.rb
+++ spec/requests/api/v1/customers_spec.rb
@@ -4,7 +4,7 @@ describe "Customers API" do
   it "sends a list of customers" do
     create_list(:customer, 3)
 
-    get 'api/v1/customers'
+    get '/api/v1/customers'
 
     expect(response).to be_success
   end
