79f89c765c5c0853fe026fafa5fd267ea4895961
Benjamin Ross <benjaminross1001@gmail.com>
2017-06-26 18:24:57 -0600

    generates transaction model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627000106) do
+ActiveRecord::Schema.define(version: 20170627002402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20170627000106) do
 
   create_table "merchants", force: :cascade do |t|
     t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.text "credit_card_number"
+    t.date "credit_card_expiration_date"
+    t.integer "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
