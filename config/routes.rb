fafaa9e780826005d37de3438a126ec51886536e
Sarah Kirk <kirk710@gmail.com>
2017-06-28 14:02:22 -0600

    adds rand endpoint for transaction

--- config/routes.rb
+++ config/routes.rb
@@ -7,22 +7,22 @@ Rails.application.routes.draw do
         get '/:id/items', to: 'items#index'
       end
       resources :merchants, only: [:index, :show]
-      resources :customers, only: [:index, :show]
       namespace :customers do
         get '/:id/invoices', to: 'invoices#index', as: 'invoices'
         get '/:id/transactions', to: 'transactions#index', as: 'transactions'
         get '/:id/favorite_merchant', to: 'favorite_merchant#show'
       end
-      resources :transactions, only: [:index, :show]
+      resources :customers, only: [:index, :show]
       namespace :transactions do
         get '/:id/invoice', to: 'invoices#index', as: 'invoices'
+        get '/random', to: 'random#show'
       end
-      resources :items, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
       namespace :items do
         get '/:id/invoice_items', to: 'invoice_items#index', as: "invoice_items"
         get '/:id/merchant', to: 'merchants#show', as: 'merchant'
       end
-      resources :invoices, only: [:index, :show]
+      resources :items, only: [:index, :show]
       namespace :invoices do
         get '/:id/items', to: 'items#index'
         get '/:id/transactions', to: 'transactions#index'
@@ -30,11 +30,12 @@ Rails.application.routes.draw do
         get '/:id/customer', to: 'customers#index'
         get '/:id/merchant', to: 'merchants#index'
       end
-      resources :invoice_items, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
       namespace :invoice_items do
         get '/:id/item', to: 'items#index'
         get '/:id/invoice', to: 'invoices#index'
       end
+      resources :invoice_items, only: [:index, :show]
     end
   end
 end
