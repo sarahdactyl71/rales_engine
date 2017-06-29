7cf3eb0eb4b4231199602849d8e8e188933a7017
Sarah Kirk <kirk710@gmail.com>
2017-06-29 14:59:04 -0600

    starts working on favorite merchant biz intel"

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,10 @@ class Customer < ApplicationRecord
   has_many :merchants, through: :invoices
 
   def favorite_merchant
-    #binding.pry
+    self.invoices
+    .joins(:merchant, :transactions)
+    .where(transactions: {result: 0})
+    .group(:merchant_id)
+    .order
   end
 end
