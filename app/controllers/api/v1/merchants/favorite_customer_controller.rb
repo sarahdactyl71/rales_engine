7b04692afcbcb83e3cf83366675fc69accf7ae96
Benjamin Ross <benjaminross1001@gmail.com>
2017-06-29 15:21:22 -0600

    finishes merchant favorite customer biz intel

---
+++ app/controllers/api/v1/merchants/favorite_customer_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Merchants::FavoriteCustomerController < ApplicationController
+
+  def show
+    render json: Merchant.find(params[:id]).favorite_customer
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         get '/random', to: 'random#show'
         get '/most_revenue', to: 'most_revenue#index'
         get '/most_items', to: 'most_items#index'
+        get '/:id/favorite_customer', to: 'favorite_customer#show'
         get '/:id/revenue', to: 'revenue#show'
         get '/revenue', to: 'revenue#index'
         get '/:id/invoices', to: 'invoices#index'
